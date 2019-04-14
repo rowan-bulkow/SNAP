7acf3db79b91f35a7195098aae823e03854bad29
rowan-bulkow <rowan.bulkow@gmail.com>
2019-04-13 20:26:15 -0800

    Write bash script to make gephi-pipe development easier

---
+++ GephiPipe/build.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+#
+# Builds and runs the GephiPipe application.
+# Runs in same directory as java files.
+# Outputs to testOutput/ directory.
+# Requires gephi-toolkit.jar and org-gephi-layout-plugin-circularlayout.jar in lib/ directory:
+# - build.sh
+# - AutoGephiPipe.java
+# - Main.java
+# - lib
+#   - gephi-toolkit.jar
+#   - org-gephi-layout-plugin-circularlayout.jar
+# - testOutput
+#   - test.gexf
+#
+# Usage
+#
+# build.sh [command] [params]
+#
+# Where command is one of the following:
+#
+# run - runs gephi-pipe.jar, requires input path, allows params: layout, modResolution
+#   build.sh run inputPath [layout modResolution]
+#
+# build - compiles java files, bundles into gephi-pipe.jar, does not take any params
+#   build.sh build
+#
+# buildandrun - builds, then runs. Requires and allows the same arguments that run does.
+#   build.sh buildandrun inputPath [layout modResolution]
+#
+# Params:
+# inputPath - folder containing input .dl files
+# layout - desired layout for graph. Default: 0, Circular Star Layout. Possible values:
+#     0 - Circular star layout
+#     1 - Radial axis layout
+#     2 - Yifan Hu layout
+#     3 - Force Atlas layout
+# modResolution - desired mod resolution. Default: 0.4
+#
+
+build() {
+    echo "Building..."
+    javac -cp .:lib/gephi-toolkit.jar:lib/org-gephi-layout-plugin-circularlayout.jar *.java
+    if [[ $? == 0 ]]; then
+        sudo jar cfmv ./gephi-pipe.jar MANIFEST.MF *.class
+        if [[ $? == 0 ]]; then
+            echo "Successfully built."
+            return 0
+        fi
+    fi
+    echo "Error: Problem building."
+    return 1
+}
+
+run() {
+    java -jar gephi-pipe.jar $1 $2 $3
+    if [[ $? == 0 ]]; then
+        return 0
+    fi
+    echo "Error: Problem running."
+    return 1
+}
+
+command=$1
+
+if [[ "$command" == "run" ]]; then
+    if [[ $# < 2 || $# > 4 ]]; then
+        echo "Error: Invalid number of arguments for run."
+        exit 1
+    fi
+
+    input=$2
+    layout="0"
+    modResolution="0.4"
+    if [[ $# == 3 ]]; then
+        layout=$3
+    elif [[ $# == 4 ]]; then
+        layout=$3
+        modResolution=$4
+    fi
+    run $input $layout $modResolution
+elif [[ "$command" == "build" ]]; then
+    build
+elif [[ "$command" == "buildandrun" ]]; then
+    build
+
+    if [[ $# < 2 || $# > 4 ]]; then
+        echo "Error: Invalid number of arguments for run."
+        exit 1
+    fi
+
+    input=$2
+    layout="0"
+    modResolution="0.4"
+    if [[ $# == 3 ]]; then
+        layout=$3
+    elif [[ $# == 4 ]]; then
+        layout=$3
+        modResolution=$4
+    fi
+    run $input $layout $modResolution
+else
+    echo "Error: Command not recognized."
+fi
